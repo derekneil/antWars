1844832531e33d2c5aadace66f0659acb256fa33
Derek Neil <derekrneil@gmail.com>
2014-09-20 13:09:56 -0300

    some _compileAll stuff i was trying that didn't work

--- scripts/_compileAll.sh
+++ scripts/_compileAll.sh
@@ -1,4 +1,4 @@
-javac ../src/gameboard/*.java
-javac ../src/visualization/*.java
-javac ../src/antServer/*.java
-javac ../src/antClient/*.java
+javac -classpath ../src:../src/antServer:../src/antClient:../src/gameboard:../src/visualization ../src/gameboard/*.java
+javac -classpath ../src:../src/antServer:../src/antClient:../src/gameboard:../src/visualization ../src/visualization/*.java
+javac -classpath ../src:../src/antServer:../src/antClient:../src/gameboard:../src/visualization ../src/antServer/*.java
+javac -classpath ../src:../src/antServer:../src/antClient:../src/gameboard:../src/visualization ../src/antClient/*.java
